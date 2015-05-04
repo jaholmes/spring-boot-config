bda6c7eda923d9ffebeff56eacb58171625f8196
jholmes-pivotal <jholmes@pivotal.io>
2015-05-03 21:06:04 -0700

    Set active profile in test

--- src/test/java/org/jeff/SpringBootConfigApplicationTests.java
+++ src/test/java/org/jeff/SpringBootConfigApplicationTests.java
@@ -2,6 +2,7 @@ package org.jeff;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootConfigApplication.class)
 @WebAppConfiguration
+@ActiveProfiles("local")
 public class SpringBootConfigApplicationTests {
 
 	@Test
